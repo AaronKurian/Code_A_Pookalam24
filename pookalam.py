901a6d8039ae5b1a355b14e7631c2bc6c2459403
AaronKurian <aaronkurianabraham@gmail.com>
2024-09-18 22:25:34 +0530

    designed4

--- pookalam.py
+++ pookalam.py
@@ -4,6 +4,7 @@ import math
 s = turtle.Turtle()
 
 #happy onam
+s.speed(0)
 penup()
 goto(-700,320)
 pendown()
@@ -11,7 +12,7 @@ color("red")
 write("Happy Onam!",font=("Times New Roman",50,"bold"))
 
 #submitted by
-color("#6c55ed")
+s.speed(0)
 penup()
 goto(350,-300)
 pendown()
@@ -25,6 +26,7 @@ write("- CS3A",font=("Times New Roman",20))#, "italic"))
 
 
 def designline():
+    s.speed(0)
     s.penup()
     s.shapesize(stretch_wid=0.75, outline=None)  # Adjust size 
     s.goto(-700,305)
@@ -41,8 +43,6 @@ def designline():
 def draw_simple_circle(radius, color, fill_color):
     s = turtle.Turtle()
     s.speed(0)
-    #screen = turtle.Screen()
-    #screen.bgcolor("white")  # Optional: set background color
     s.penup()
     s.goto(0, -radius)  
     s.pendown() 
@@ -54,6 +54,7 @@ def draw_simple_circle(radius, color, fill_color):
 
    
 def tripatt(len,clr):
+    s.speed(0)
     s.color(clr)
     for i in range(36):
         s.begin_fill()
@@ -71,8 +72,9 @@ def tripatt(len,clr):
 
 
 
-s.speed(0)
+
 def draw_rectangle1(x, y, width, height, color1,color2, angle=0):
+    s = turtle.Turtle()
     s.speed(0)
     s.pencolor(color1)
     s.pensize(3)
@@ -93,6 +95,7 @@ def draw_rectangle1(x, y, width, height, color1,color2, angle=0):
     s.end_fill()
 
 def draw_rectangle2(x, y, width, height, color1,color2, angle=0):
+    s = turtle.Turtle()
     s.speed(0)
     s.pencolor(color1)
     s.pensize(3)
@@ -112,6 +115,7 @@ def draw_rectangle2(x, y, width, height, color1,color2, angle=0):
     s.end_fill()
 
 def draw_rectangle3(x, y, width, height, color1,color2, angle=0):
+    s = turtle.Turtle()
     s.speed(0)
     s.pencolor(color1)
     s.pensize(3)
@@ -130,6 +134,8 @@ def draw_rectangle3(x, y, width, height, color1,color2, angle=0):
     s.end_fill()
 
 def draw_polygon(points, color1,color2):
+    s = turtle.Turtle()
+    s.speed(0)
     s.penup()
     s.pencolor(color1)
     s.goto(points[0])
@@ -142,6 +148,7 @@ def draw_polygon(points, color1,color2):
     s.end_fill()
 
 def mec():
+    s = turtle.Turtle()
     s.speed(0)
     for i in range(3):
         draw_rectangle1(15, -20 + i * 10, 30, 10, "#8d6b48","#e8c9ab")
@@ -152,9 +159,7 @@ def mec():
 
 def draw_circle_with_colored_divisions(radius, divisions, color1, color2):
     s = turtle.Turtle()
-    s.speed(0)  # Fastest drawing speed
-
-    # Draw the circle outline
+    s.speed(0)
     s.penup()
     s.goto(0, -radius)
     s.pendown()
@@ -182,40 +187,31 @@ def draw_circle_with_colored_divisions(radius, divisions, color1, color2):
 
 def draw_circle_with_colored_divisions1(radius, divisions, colors):
     s = turtle.Turtle()
-    s.speed(0)  # Fastest drawing speed
-
-    # Draw the circle outline
+    s.speed(0) 
     s.penup()
-    s.goto(0, -radius)  # Move the turtle to the starting point of the circle
+    s.goto(0, -radius) 
     s.pendown()
     s.circle(radius)
-
-    # Draw and color the divisions
-    angle = 360 / divisions  # Angle between each division line
+    angle = 360 / divisions  
     for i in range(divisions):
         s.penup()
-        s.goto(0, 0)  # Move the turtle to the center of the circle
+        s.goto(0, 0) 
         s.pendown()
-        
-        # Set the fill color from the list of 6 colors
         s.color(colors[i % len(colors)])
         s.begin_fill()
-        
-        # Draw the sector
-        s.setheading(90)  # Point the turtle upwards
-        s.right(angle * i)  # Rotate the turtle to the correct angle
-        s.forward(radius)  # Draw the line to the circumference
-        s.left(90)  # Turn left to face the circle edge
-        s.circle(radius, angle)  # Draw the arc of the sector
-        s.left(90)  # Turn left to return to the center
-        s.forward(radius)  # Complete the triangle to the center
+        s.setheading(90) 
+        s.right(angle * i) 
+        s.forward(radius)  
+        s.left(90) 
+        s.circle(radius, angle)  
+        s.left(90)  
+        s.forward(radius)  
         
         s.end_fill()
 
-colors = ['#ffffff', '#ffba08', '#f48c06', '#dc2f02', '#d00000', '#9d0208']  # 6 different colors
-
 
 def design():
+    s.speed(0)
     s.goto(0,-392)
     s.shapesize(stretch_wid=1, outline=None)
     s.shape("circle")
@@ -228,6 +224,7 @@ def design():
 designline()
 design()
 draw_simple_circle(385, "#6A040F", "#6A040F")
+'''
 draw_simple_circle(370, "#9D0208", "#9D0208")
 tripatt(360,"#DC2F02")
 tripatt(327, "#E85D04")
@@ -236,26 +233,26 @@ tripatt(268,"#FFBA08")
 tripatt(245, "white")
 tripatt(225,"#7D82B8")
 tripatt(205,"#613F75")
-tripatt(185,"#03071e")
-
+tripatt(185,"#03071e")'''
 draw_simple_circle(167, "white", "#fff9ec")
 draw_simple_circle(140, "black", "#2b2d42")
-draw_circle_with_colored_divisions(138, 8, '#610b01', '#002107')
-draw_circle_with_colored_divisions(118, 8, '#E85D04', '#01330c')
-
+colors = ['#610b01', '#002107']  # 2 different colors
+draw_circle_with_colored_divisions1(138, 8, colors)
+colors = ['#E85D04', '#01330c']  # 2 different colors
+draw_circle_with_colored_divisions1(118, 8, colors)
 draw_simple_circle(98, "yellow", "yellow")
 draw_simple_circle(78, "white", "white")
-
-
+colors = ['#ffffff', '#ffba08', '#f48c06', '#dc2f02', '#d00000', '#9d0208']  # 6 different colors
 draw_circle_with_colored_divisions1(70, 24, colors)
 colors = ['#ffba08', '#f48c06', '#dc2f02', '#d00000', '#9d0208','#ffffff']  # 6 different colors
 draw_circle_with_colored_divisions1(60, 24, colors)
 colors = ['#f48c06', '#dc2f02', '#d00000', '#9d0208','#ffffff','#ffba08' ]  # 6 different colors
 draw_circle_with_colored_divisions1(50, 24, colors)
 draw_simple_circle(40, "white", "white")
-colors = ['#38b000', '#70e000']# '#dc2f02', '#d00000', '#9d0208']  # 6 different colors
+colors = ['#38b000', '#70e000']# 2 different colors
 draw_circle_with_colored_divisions1(40, 12, colors)
-# Run the mec() function
 mec()
+
+s.hideturtle()
 
 turtle.Screen().exitonclick()
